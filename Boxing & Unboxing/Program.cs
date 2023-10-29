952e3e39771f420ef1178bbaf902d10d9b139b74
Burhanaddinzm <burhanaddinzm@code.edu.az>
2023-10-29 12:12:28 +0400

    Boxing & Unboxing

---
+++ Boxing & Unboxing/Program.cs
@@ -0,0 +1,12 @@
+﻿int num = 256;
+//Boxing:
+object numObject = num;
+//Changing the num's value doesn't affect numObject's stored value:
+num = 512;
+Console.WriteLine(num);
+Console.WriteLine(numObject);
+//Unboxing:
+int num2 = (int)numObject;
+Console.WriteLine(num2);
+//Can't use different types when unboxing:
+//int num3 = (string)numObject;
